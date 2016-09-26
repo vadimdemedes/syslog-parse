d641614656f5fc99308d35f97ad32df29dc0f011
rentzsch <jwr.git@redshed.net>
2016-09-26 14:50:36 -0700

    test: add failing test for process with numbers, periods and slashes

--- test.js
+++ test.js
@@ -105,4 +105,24 @@ describe ('syslog-parse', function () {
     
     Object.keys(log).length.should.equal(0);
   });
+
+  it ('parse process with slash', function () {
+    const message = '<38>Feb 07 01:02:03 abc docker/6e6ea36be53a[253]: Listening at 0.0.0.0:3000';
+
+    const log = parse(message);
+    log.priority.should.equal(38);
+    log.facilityCode.should.equal(4);
+    log.facility.should.equal('auth');
+    log.severityCode.should.equal(6);
+    log.severity.should.equal('info');
+    log.time.getMonth().should.equal(1);
+    log.time.getDate().should.equal(7);
+    log.time.getHours().should.equal(1);
+    log.time.getMinutes().should.equal(2);
+    log.time.getSeconds().should.equal(3);
+    log.host.should.equal('abc1');
+    log.process.should.equal('docker/6e6ea36be53a');
+    log.pid.should.equal(253);
+    log.message.should.equal('Listening at 0.0.0.0:3000');
+  });
 });
